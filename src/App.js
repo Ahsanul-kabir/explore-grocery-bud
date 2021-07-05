ccc924f9abf215dcd168d1862b283c3e60ee7423
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-07-05 12:29:02 +0600

    main stucture

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Alert from './components/Alert'
+import List from "./components/List";
+
 
 function App() {
+  const [name, setName] = useState('')
+  const [list, setList] = useState([])
+  const [isEditing, setIsEditing] = useState(false)
+  const [editId, setEditId] = useState(null)
+  const [alert, setAlert] = useState({ show: false, msg: '', type: '' })
+
+  const handleSubmit =(e)=>{
+    e.preventDeault()
+    console.log('hello')
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section-center">
+      <form onSubmit={handleSubmit} className="grocery-form">
+        {alert.show && <Alert/>}
+        <h3>Grocery Bird</h3>
+        <div className="form-control">
+          <input type="text" className='grocery' placeholder='e.g. eggs'
+          value={name}
+          onChange={(e)=>setName(e.target.value)}
+          />
+
+          <button type="submit" className='submit-btn'>
+            {isEditing ? 'edit': 'submit'}
+          </button>
+        </div>
+      </form>
+      <div className="grocery-container">
+        <List />
+
+        <button className='clear-btn'></button>
+      </div>
+    </section>
   );
 }
 
---
+++ src/components/Alert.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Alert = () => {
+    return (
+        <div>
+            <h4>Alert Js</h4>
+        </div>
+    );
+};
+
+export default Alert;
