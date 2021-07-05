d47ef80542aab34f5695ac6952b0a4812905394d
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-07-05 21:17:58 +0600

    add item part

--- src/App.js
+++ src/App.js
@@ -10,31 +10,45 @@ function App() {
   const [editId, setEditId] = useState(null)
   const [alert, setAlert] = useState({ show: false, msg: '', type: '' })
 
-  const handleSubmit =(e)=>{
-    e.preventDeault()
-    console.log('hello')
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (!name) {
+      // display alert
+    }
+    else if (name && isEditing) {
+      // editing
+    }
+    else {
+      // show alert
+      const newItem = { id: new Date().getTime.toString(), title: name };
+      setList([...list, newItem]);
+      setName('')
+    }
   }
   return (
     <section className="section-center">
       <form onSubmit={handleSubmit} className="grocery-form">
-        {alert.show && <Alert/>}
+        {alert.show && <Alert />}
         <h3>Grocery Bird</h3>
         <div className="form-control">
           <input type="text" className='grocery' placeholder='e.g. eggs'
-          value={name}
-          onChange={(e)=>setName(e.target.value)}
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
 
           <button type="submit" className='submit-btn'>
-            {isEditing ? 'edit': 'submit'}
+            {isEditing ? 'edit' : 'submit'}
           </button>
         </div>
       </form>
-      <div className="grocery-container">
-        <List />
-
-        <button className='clear-btn'></button>
-      </div>
+      {
+        list.length > 0 && (
+          <div className="grocery-container">
+            <List items={list} />
+            <button className='clear-btn'>Clear Items</button>
+          </div>
+        )
+      }
     </section>
   );
 }
